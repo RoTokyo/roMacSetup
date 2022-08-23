47534c030a44c26cf8cf843b3271a7f32b0477e8
roTokyo <roTokyo@icloud.me>
2022-08-23 11:35:47 +0900

    Small changes

--- zz-bin/1to1_photo_metadata_management.sh
+++ zz-bin/1to1_photo_metadata_management.sh
@@ -17,17 +17,23 @@ echo ''
   EXTENTION="${READIMGFILENAME##*.}"
   FILENAME="${READIMGFILENAME%.*}"
   FILEPATH=$(dirname "$READIMGFILENAME")
+  ORIGINIMGPATH=$(pwd)
+
+  echo ${IMGFILENAME}
+  echo ${EXTENTION}
+  echo ${FILENAME}
+  echo ${FILEPATH}
+
 
 if [[ "${IMGFILENAME}" != "" && -f "${ORIGINIMGPATH}/${READIMGFILENAME}" ]]; then
 
-  if [[ "${FILEPATH}" == "" ]]; then
-    ORIGINIMGPATH=$ORIGINIMGPATH
+  if [[ "${FILEPATH}" == "." ]]; then
+    ORIGINIMGPATH=$(pwd)
+    echo "Si:  " ${ORIGINIMGPATH}
     else
       ORIGINIMGPATH="${ORIGINIMGPATH}/${FILEPATH}"
+      echo "No:  " ${ORIGINIMGPATH}
   fi
-
-  read -p 'Insert photo title (*): ...' IMGTITLE
-  echo ''
 
   # erase all image metadata
   # -ProfileDescription="sRGB IEC61966-2.1"
@@ -47,38 +53,67 @@ if [[ "${IMGFILENAME}" != "" && -f "${ORIGINIMGPATH}/${READIMGFILENAME}" ]]; the
       -ISO \
       -FocalLength \
       -LensModel \
+      -LensID \
       ${ORIGINIMGPATH}/${IMGFILENAME}
+
+    # exiftool '-LensModel+<LensID' ${ORIGINIMGPATH}/${IMGFILENAME}
+
   fi
 
 # write copyrights data
 # Modifiche:
 # -LensModel="Fujifilm XF 18-55mm F2.8-4 R LM OIS" \
 # -LensModel="Sigma 30mm F1.4 DC HSM Art" \
-# -LensModel="Nikon 105mm F2.8D Micro Nikkor" \
 # -LensModel="Sigma 105mm F2.8 DG Macro HSM" \
+# -LensModel="Nikon AF Zoom-Nikkor 70-300mm f/4-5.6D ED"
 
 # -CreatorWorkEmail="roberto.calesini@gmail.com" \
 # -CreatorWorkURL="" \
 
+  CAMERA=$(exiftool -Make ${ORIGINIMGPATH}/${IMGFILENAME})
+
+  if [[  "${CAMERA}" =~ .*"SIGMA".* ]]; then
+    echo "1): Sigma 30mm F1.4 DC HSM Art"
+    echo "2): Sigma 105mm F2.8 DG Macro HSM"
+    echo ""
+    echo -n "Which lens model? [ 1 or 2]: "
+    read READREPLY
+    case $READREPLY in
+      [1] ) LENSMODEL="Sigma 30mm F1.4 DC HSM Art"
+        ;;
+      [2] ) LENSMODEL="Sigma 105mm F2.8 DG Macro HSM"
+        ;;
+      *) echo "Invalid input"
+        exit 0
+        ;;
+    esac
+    exiftool -LensModel="${LENSMODEL}" ${ORIGINIMGPATH}/${IMGFILENAME}
+  fi
+
+  read -p 'Insert photo title (*): ...' IMGTITLE
+  echo ''
+
   exiftool \
     -PhotoTitle="${IMGTITLE}" \
     -Creator="Roberto Calesini" \
-    -Rights="© 2013 Roberto Calesini. All rights reserved." \
+    -Rights="© 2013 Roberto Calesini aka roTokyo. All rights reserved." \
   	-Artist="roTokyo" \
   	-ArtistCity="Tokyo" \
   	-ArtistCountry="Japan" \
     -ArtistWorkURL="https://www.flickr.com/photos/rotokyo/albums" \
     -ArtistWorkEmail="rotokyo@icloud.com" \
-    -CopyrightsOwner="© 2013 Roberto Calesini aka roTokyo" \
+    -CopyrightsOwner="© 2013 Roberto Calesini aka roTokyo. All rights reserved." \
     -UsageTermsStatement="For evaluation only, no reproduction is permitted without written permission by copyrights owner." \
     -CopyrightFlag=true \
     ${ORIGINIMGPATH}/${IMGFILENAME}
+  
+  
 
-  exiftool -a -G1 -s -H ${ORIGINIMGPATH}/${IMGFILENAME}
+  exiftool -a -G1 -s -H -sort ${ORIGINIMGPATH}/${IMGFILENAME}
 
 else
   echo ''
-  echo 'Something went wrong ... retry!'
+  echo 'File not found ... retry!'
 fi
 
-
+unset READIMGFILENAME IMGFILENAME EXTENTION FILENAME FILEPATH ORIGINIMGPATH IMGTITLE READREPLY CAMERA LESNMODEL
